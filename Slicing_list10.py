5a155b3f266c27f65d924f593896e7a71f68ae36
sobirismatov <name@uzcoder96@gmail.com>
2022-11-08 12:38:47 +0500

    10

--- Slicing_list10.py
+++ Slicing_list10.py
@@ -1,10 +1,11 @@
 def main(list1,n):
     """
-    A list of several elements is given. Return all elements in reverse order except n elements from the beginning.
+      A list of several elements is given. Return all items except n elements from the beginning.
     Args:
         list1(list): parameter
         n(int): parameter
     Returns:
         list: return answer.
     """
-    return
+    return list1[:n-1:-1]
+print(main([1,2,3,5,6,7],2))
